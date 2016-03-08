ca4f71446ecbebde4bdec119d622b3c37fc52eb8
al-sade <alsade15@gmail.com>
2016-03-08 11:23:41 +0200

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,8 @@
 <?php
 
-$DB_HOST = 'localhost';
-$DB_USER = 'root';
-$DB_PASS = 'alaba1515';
-$DB_NAME = 'db_studio_list';
+$DB_HOST = HOST;
+$DB_USER = USER;
+$DB_PASS = PASS;
+$DB_NAME = DB;
 $mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 ?>
